76394d7f23e792418db96b8a4525d27593beda51
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-05-25 10:59:32 -0400

    Retornar elementos

---
+++ src/PrimeraApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PrimeraApp = ()=> {
+
+    return (
+        <> 
+            <h1>Hola Mundo</h1>
+            <p>Mi primera aplicacion</p>
+        </>
+       
+    );
+}
+
+export default PrimeraApp;
