7e514c65171b9b4d6dbf02e1fc00f1ebaf307bdf
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-05-26 12:47:07 -0400

    PropTypes

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PrimeraApp = ( props )=> {
-
-    const saludo = 'Hola Mundo';
+const PrimeraApp = ({ saludo })=> {
 
     return (
         <> 
-            <h1> { props.saludo } </h1>
+            <h1> { saludo } </h1>
             <p>Mi primera aplicacion</p>
         </>
        
     );
+}
+
+PrimeraApp.propTypes = {
+     saludo: PropTypes.string.isRequired
 }
 
 export default PrimeraApp;
