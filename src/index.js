f3749e4019ac935b849e99cc61d822a1de8f850e
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-05-26 19:13:58 -0400

    react 18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom';
 import CounterApp from './CounterApp';
 //import PrimeraApp from './PrimeraApp';
 
 
 const divRoot = document.querySelector('#root');
-
-ReactDOM.render(<CounterApp value={ 10 } />, divRoot);
+const root = createRoot(divRoot);
+root.render(<CounterApp value={ 10 } />);
